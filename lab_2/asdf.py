b4763e0257c81f7b924203ba97250695216476ff
Demix361 <ti-aminov@yandex.ru>
2020-10-17 00:09:15 +0300

    working???

--- lab_2/asdf.py
+++ lab_2/asdf.py
@@ -1,4 +1,20 @@
-a = [0, 1, 2, 3, 4, 5]
+'''
 
-for i in range(len(a) - 1, -1, -1):
-    print(a[i])
+s = 4
+a = [0, 0, 0]
+
+for i in range(64):
+	print(a)
+	for j in range(3):
+		if a[j] == s - 1:
+			a[j] = -1
+			a[j] += 1
+		else:
+			a[j] += 1
+			break
+'''
+
+a = [45, 2, 67, 23, 11]
+
+for i in range(50):
+	print(a[2 - (i % len(a))])
--- lab_2/main.py
+++ lab_2/main.py
@@ -1,4 +1,4 @@
-from random import sample
+from random import sample, randint
 
 
 class Rotor:
@@ -6,6 +6,17 @@ class Rotor:
         self.alphabet = alphabet
         self.pos = 0
         self.symbols = sample(self.alphabet, k=len(self.alphabet))
+
+    def forward(self, symbol):
+        return self.symbols[self.alphabet.index(symbol) - self.pos]
+
+    def backward(self, symbol):
+        return (self.symbols.index(symbol) + self.pos) % len(self.alphabet)
+
+    def roll(self):
+        self.pos += 1
+        if self.pos == len(self.alphabet):
+            self.pos = 0
 
 
 class Deflector:
@@ -19,26 +30,76 @@ class Deflector:
         for i in range(len(self.alphabet) - 1, -1, -1):
             self.symbols[i] = self.alphabet[len(self.alphabet) - 1 - i]
 
+    def process(self, symbol):
+        return self.symbols[self.alphabet.index(symbol)]
+
 
 def encode(rotors, deflector, message):
-    for rotor in rotors:
-        
+    encrypted_message = []
+    s = len(rotors[0].alphabet)
 
+    for sym in message:
+        print(f'[{sym}]', end=' -> ')
+
+        for rotor in rotors:
+            sym = rotor.forward(sym)
+            print(f'{sym}', end=' -> ')
+
+        sym = deflector.process(sym)
+        print(f'| {sym}', end=' --> ')
+
+        for i in range(len(rotors) - 1, -1, -1):
+            sym = rotors[i].backward(sym)
+            if i != 0:
+                print(f'{sym}', end=' -> ')
+            else:
+                print(f'{sym}')
+
+        for j in range(len(rotors)):
+            if rotors[j].pos == s - 1:
+                rotors[j].pos = -1
+                rotors[j].pos += 1
+            else:
+                rotors[j].pos += 1
+                break
+
+        encrypted_message.append(sym)
+
+    return encrypted_message
 
 
 def main():
-    alphabet = [i for i in range(20)]
+    alph_size = 10
+    alphabet = [i for i in range(alph_size)]
 
-    message = [2, 2, 44, 3, 32, 45, 230, 1, 193]
+    message = [3, 4, 8, 5, 0]
+    message = [6, 7, 5, 0, 7]
+    #message = [randint(0, alph_size - 1) for i in range(5)]
+
 
     rotor_1 = Rotor(alphabet)
     rotor_2 = Rotor(alphabet)
     rotor_3 = Rotor(alphabet)
+    # rotor_1.symbols = [1, 0, 3, 8, 6, 4, 2, 5, 7, 9]
+    # rotor_2.symbols = [5, 0, 1, 2, 8, 4, 7, 3, 9, 6]
+    # rotor_3.symbols = [1, 8, 2, 0, 3, 9, 5, 6, 4, 7]
+    rotor_1.symbols = [9, 0, 1, 2, 3, 4, 5, 6, 7, 8]
+    rotor_2.symbols = [4, 5, 6, 7, 8, 9, 0, 1, 2, 3]
+    rotor_3.symbols = [7, 8, 9, 0, 1, 2, 3, 4, 5, 6]
     deflector = Deflector(alphabet)
 
     rotors = [rotor_1, rotor_2, rotor_3]
+    print(rotor_1.symbols)
+    print(rotor_2.symbols)
+    print(rotor_3.symbols)
+    #print(deflector.symbols)
 
-    print(deflector.symbols)
+    print()
+    print(message)
+
+    new_mes = encode(rotors, deflector, message)
+
+    print(new_mes)
 
 
 
