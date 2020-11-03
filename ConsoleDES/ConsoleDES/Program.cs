134fa483dac8a4cc7ef6c79ee40721f7672a0e54
Demix361 <ti-aminov@yandex.ru>
2020-11-03 15:55:10 +0300

    lab 3 started

---
+++ ConsoleDES/ConsoleDES/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleDES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DES des = new DES();
+
+            string inFilename = @"D:\Libraries\Music\hello.txt";
+            string outFilename = @"D:\Libraries\Music\hello_dec.txt";
+
+            FileStream inF = new FileStream(inFilename, FileMode.Open);
+            FileStream outF = new FileStream(outFilename, FileMode.Create);
+
+            des.encrypt(inF, outF);
+
+            inF.Close();
+            outF.Close();
+
+        }
+    }
+}
