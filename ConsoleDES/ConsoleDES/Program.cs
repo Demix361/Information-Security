822be14913dbd4d972b78df29001218e460fe429
Demix361 <ti-aminov@yandex.ru>
2020-11-04 18:07:55 +0300

    working?

--- ConsoleDES/ConsoleDES/Program.cs
+++ ConsoleDES/ConsoleDES/Program.cs
@@ -9,8 +9,11 @@ namespace ConsoleDES
         {
             DES des = new DES();
 
-            string inFilename = @"D:\Libraries\Music\hello.txt";
-            string outFilename = @"D:\Libraries\Music\hello_dec.txt";
+            string basepath = @"D:\Libraries\Music\source\";
+            string filetype = ".png";
+            string inFilename = basepath + "emoji" + filetype;
+            string outFilename = basepath + "FILE_enc" + filetype;
+            string outFilename2 = basepath + "FILE_dec" + filetype;
             string key = "1111000011110000110011000011001111110000111100001100110000110011";
 
             FileStream inF = new FileStream(inFilename, FileMode.Open);
@@ -20,6 +23,16 @@ namespace ConsoleDES
 
             inF.Close();
             outF.Close();
+
+            
+            inF = new FileStream(outFilename, FileMode.Open);
+            outF = new FileStream(outFilename2, FileMode.Create);
+
+            des.decrypt(inF, outF, key);
+
+            inF.Close();
+            outF.Close();
+            
 
         }
     }
